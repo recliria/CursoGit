acf943af937055aa1820d539e5772134dd41fb71
raul <recliria@gmail.com>
2023-11-08 11:08:48 -0800

    Se agregó la función Resta

--- functions.py
+++ functions.py
@@ -1,9 +1,18 @@
-def sumar_dos_numeros():
+def sum():
     number_one = 5
     number_two = 6
 
     result = number_one + number_two
 
-    print("The result is ", result)
+    print("The result of sum is ", result)
+
+def rest():
+    number_one = 8
+    number_two = 4
+
+    result = number_one - number_two
+
+    print("The result of rest is:",result)
     
-sumar_dos_numeros()
+sum()
+rest()
